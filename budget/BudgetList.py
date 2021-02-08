0304182bb514ef7da802a52429d9557e3788fe04
Taylynne <theflowerbloomed@gmail.com>
2021-02-07 22:11:34 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -30,7 +30,7 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
-    print("The count of all expenses: " + str(myBudgetList.__len__()))
+    print("The count of all expenses: " + str(len(myBudgetList)))
 
 if __name__ == "__main__":
     main()
