7ae9ca7e253452eb8f48b13e72e6a6060a1bd85e
Taylynne <theflowerbloomed@gmail.com>
2021-02-07 22:36:10 -0500

    Update BudgetList.py
    fixed __next__

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -28,9 +28,9 @@ class BudgetList():
 
     def __next__(self):
         try:
-            return self.__next__(self.iter_e)
+            return self.iter_e.__next__()
         except StopIteration as stop:
-            return self.__next__(self.iter_o)
+            return self.iter_o.__next__()
 
 
 
