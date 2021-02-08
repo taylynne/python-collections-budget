a1a6b75941a884e0780d6d83b704d063f01a2b15
Taylynne <theflowerbloomed@gmail.com>
2021-02-07 23:31:25 -0500

    Finished Module 5

--- budget/Expense.py
+++ budget/Expense.py
@@ -47,7 +47,7 @@ class Expenses():
                                 x.category == 'Classes'  or x.category == 'Utilities' or
                                 x.category == 'Mortgage'}
 
-        food_expenses = {x for x in self.list 
+        food_expenses = {x for x in self.list
                             if x.category == 'Groceries' or x.category == 'Eating Out'}
 
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
