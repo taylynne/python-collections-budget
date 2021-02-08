32b02c49897c6651e976a3739ae13e7e76527b67
Taylynne <theflowerbloomed@gmail.com>
2021-02-07 22:34:25 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,4 +1,5 @@
 from . import Expense
+import matplotlib.pyplot as plt
 
 class BudgetList():
 
@@ -46,6 +47,16 @@ def main():
 
     for entry in myBudgetList:
         print(entry)
+
+    fig, ax = plt.subplots()
+
+    labels = ["Expenses", "Overages", "Budget",]
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+
+    ax.bar(labels, values, color=['green', 'red', 'blue',])
+    ax.set_title("Your total expenses vs. total budget")
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
